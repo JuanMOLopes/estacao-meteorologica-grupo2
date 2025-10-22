a598ffeae15ded90c71782f0408cadbee152bb5b
Zayra <Zayrafranca5@gmail.com>
2025-10-22 11:39:08 -0300

    grupo 2 parte 3

--- src/pages/Grupo.jsx
+++ src/pages/Grupo.jsx
@@ -1,6 +1,6 @@
-import "../App.css"
-import Header from "../src/components/Header/Header";
-import Footer from "../src/components/Footer/Footer";
+import "../App.css";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
 import GrupoImg from "/Grupo2Foto.jpeg";
 import TextoDireita from "../components/TextoDireita/TextoDireita";
 import TextoEsquerda from "../components/TextoEsquerda/TextoEsquerda";
@@ -9,49 +9,52 @@ function Grupo() {
   return (
     <>
       <Header titulo="Grupo 2" />
-        <img className="Grupo" src={GrupoImg} alt="Grupo 2" />
+      <img className="Grupo" src={GrupoImg} alt="Grupo 2" />
 
-        <div className="Container-grp2">
-      <p>
-        Nós do grupo 2 escrevemos esta mensagem como forma de agradecimento aos professores Antônio e Leandro por esta atividade que foi proposta. Durante o processo de criação do site, diversos pontos do grupo foram melhorados, tendo como mais importante o trabalho em equipe. Essa atividade foi de grande importância para nossa aprendizagem e estudo de conteúdos de desenvolvimento de sistemas.
-      </p>
-      <p>
-        O grupo 2 reitera sua gratidão pelo trabalho proposto pelos profissionais do ensino, em prol de obtermos conhecimento. Gratos!
-      </p>
+      <div className="Container-grp2">
+        <p>
+          Nós do grupo 2 escrevemos esta mensagem como forma de agradecimento
+          aos professores Antônio e Leandro por esta atividade que foi proposta.
+          Durante o processo de criação do site, diversos pontos do grupo foram
+          melhorados, tendo como mais importante o trabalho em equipe. Essa
+          atividade foi de grande importância para nossa aprendizagem e estudo
+          de conteúdos de desenvolvimento de sistemas.
+        </p>
+        <p>
+          O grupo 2 reitera sua gratidão pelo trabalho proposto pelos
+          profissionais do ensino, em prol de obtermos conhecimento. Gratos!
+        </p>
       </div>
 
-       <div className="secao-texto">
-          <TextoEsquerda
-            titulo="Feito por: Agatha"
-            texto="-Páginas: O projeto e perguntas e respostas"
+      <div className="secao-texto">
+        <TextoEsquerda
+          titulo="Feito por: Agatha"
+          texto="-Páginas: O projeto e perguntas e respostas"
+          imagem="/1000072743.jpg"
+        />
+        <TextoDireita
+          titulo="Feito por: Ana"
+          texto=" -Página: Resultados"
+          imagem="public/1000072785.jpg"
+        />
 
-            imagem=" "
- />
-            <TextoDireita
-            titulo="Feito por: Ana"
-            texto=" -Página: Resultados"
-            imagem=""
-          />
+        <TextoEsquerda
+          titulo="Feito por: Juan"
+          texto="-Páginas: Home e Software"
+          imagem="public/1000072746.jpg"
+        />
+        <TextoDireita
+          titulo="Feito por: Lucas"
+          texto=" -Páginas: Simulação Wokwi e Hardware"
+          imagem="public/1000072749.jpg"
+        />
 
-          <TextoEsquerda
-            titulo="Feito por: Juan"
-            texto="-Páginas: Home e Software"
-
-            imagem=" "
- />
-  <TextoDireita
-            titulo="Feito por: Lucas"
-            texto=" -Páginas: Simulação Wokwi e Hardware"
-            imagem=""
-          />
-
-           <TextoEsquerda
-            titulo="Feito por: Zayra"
-            texto="-Página:Grupo 2 "
-
-            imagem=" "
- />
-         </div>
+        <TextoEsquerda
+          titulo="Feito por: Zayra"
+          texto="-Página: Grupo 2"
+          imagem="public/1000072788.jpg"
+        />
+      </div>
       <Footer />
     </>
   );
