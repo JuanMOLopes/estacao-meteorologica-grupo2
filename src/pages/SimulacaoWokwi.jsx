14a64c357c7673eea7693b60ff28993aafa47a18
JuanMOLopes <juan.oliiveira2007@gmail.com>
2025-10-22 10:22:54 -0300

    Add Temp component with Header and Footer to multiple pages

---
+++ src/pages/SimulacaoWokwi.jsx
@@ -0,0 +1,13 @@
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+
+function Temp() {
+  return (
+    <>
+      <Header titulo="Titulo Temporario" />
+      <Footer />
+    </>
+  );
+}
+
+export default Temp;
