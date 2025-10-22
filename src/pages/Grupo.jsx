a063aff74710efc65480dab7830c3027d14e1454
Zayra <Zayrafranca5@gmail.com>
2025-10-22 10:17:07 -0300

    imagens hardware

--- src/pages/Grupo.jsx
+++ src/pages/Grupo.jsx
@@ -11,7 +11,7 @@ function Grupo() {
 
         <div className="Container-grp2">
       <p>
-        Nós do grupo 2 escrevemos esta mensagem como forma de agradecimento aos professores Julia, Antônio e Leandro por esta atividade que foi proposta. Durante o processo de criação do site, diversos pontos do grupo foram melhorados, tendo como mais importante o trabalho em equipe. Essa atividade foi de grande importância para nossa aprendizagem e estudo tanto de conteúdos de desenvolvimento de sistemas, como também de história.
+        Nós do grupo 2 escrevemos esta mensagem como forma de agradecimento aos professores Antônio e Leandro por esta atividade que foi proposta. Durante o processo de criação do site, diversos pontos do grupo foram melhorados, tendo como mais importante o trabalho em equipe. Essa atividade foi de grande importância para nossa aprendizagem e estudo tanto de conteúdos de desenvolvimento de sistemas.
       </p>
       <p>
         O grupo 2 reitera sua gratidão pelo trabalho proposto pelos profissionais do ensino, em prol de obtermos conhecimento. Gratos!
